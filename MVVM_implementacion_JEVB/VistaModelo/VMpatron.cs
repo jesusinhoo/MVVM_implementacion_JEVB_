2bc95b893e3fc9fdddcf08809db62d607ccca382
jesusinhoo <jesus.bojorquez1110@gmail.com>
2024-11-04 14:22:25 -0700

    MVVM_implementacion_JEVB

---
+++ MVVM_implementacion_JEVB/VistaModelo/VMpatron.cs
@@ -0,0 +1,38 @@
+﻿using MvvmGuia.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM_implementacion_JEVB.VistaModelo
+{
+    internal class VMpatron : BaseViewModel 
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONTRUCTOR
+
+        #endregion
+        #region OBJETOS
+        
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
